393c79749c0de61cdac3adf863a48a8205040dcd
CMS-edu <minsang4796@gmail.com>
2026-05-06 15:58:27 +0900

    Fix missions auth module dependency

--- server/src/missions/missions.module.ts
+++ server/src/missions/missions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MissionsController } from './missions.controller';
 import { MissionsService } from './missions.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [MissionsController],
   providers: [MissionsService],
   exports: [MissionsService],
